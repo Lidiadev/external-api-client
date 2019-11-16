4f1acae92b784b66e9c82214c0166b5471fe184b
Lidia <necula_lidia@yahoo.com>
2019-11-16 21:19:36 +0100

    Add UTs

---
+++ tests/RealEstate.UnitTests/Common/Extensions/PropertyMapperExtensionsTests.cs
@@ -0,0 +1,49 @@
+﻿using FluentAssertions;
+using NUnit.Framework;
+using RealEstate.Domain.Dtos;
+using RealEstate.Presentation.Common.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace RealEstate.UnitTests.Common.Extensions
+{
+    [TestFixture]
+    public class PropertyMapperExtensionsTests
+    {
+        [Test]
+        public void ToViewModel_ShouldThrowException_WhenDtoIsNull()
+        {
+            // Arrange
+            IGrouping<int, PropertyDto> dto = null;
+
+            // Act
+            // Assert
+            Assert.Throws<ArgumentNullException>(() => dto.ToViewModel());
+        }
+
+        [Test]
+        public void ToViewModel_ShouldReturnTheViewModel()
+        {
+            // Arrange
+            var properties = new List<PropertyDto>
+            {
+                new PropertyDto{ RealEstateAgentId = 111, RealEstateAgentName = "Test", Id = "1" },
+                new PropertyDto{ RealEstateAgentId = 111, RealEstateAgentName = "Test", Id = "2" },
+                new PropertyDto{ RealEstateAgentId = 111, RealEstateAgentName = "Test", Id = "3" },
+                new PropertyDto{ RealEstateAgentId = 111, RealEstateAgentName = "Test", Id = "4" }
+            };
+
+            IGrouping<int, PropertyDto> dto = properties.GroupBy(p => p.RealEstateAgentId).First();
+
+            // Act
+            var result = dto.ToViewModel();
+
+            // Assert
+            result.Should().NotBeNull();
+            result.RealEstateAgentId.Should().Be(properties.First().RealEstateAgentId);
+            result.RealEstateAgentName.Should().Be(properties.First().RealEstateAgentName);
+            result.Count.Should().Be(properties.Count());
+        }
+    }
+}
---
+++ tests/RealEstate.UnitTests/Controllers/AgentControllerTests.cs
@@ -0,0 +1,53 @@
+﻿using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using NUnit.Framework;
+using RealEstate.Presentation.Controllers;
+using RealEstate.Presentation.Models.Agent;
+using RealEstate.Presentation.Services.Interfaces;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace RealEstate.UnitTests.Controllers
+{
+    [TestFixture]
+    public class AgentControllerTests
+    {
+        private Mock<ILogger<HomeController>> _logger;
+        private Mock<IPropertyService> _propertyService;
+        private AgentController _subject;
+
+        [SetUp]
+        public void Setup()
+        {
+            _propertyService = new Mock<IPropertyService>();
+            _logger = new Mock<ILogger<HomeController>>();
+            _subject = new AgentController(_logger.Object, _propertyService.Object);
+        }
+
+        [Test]
+        public async Task Index_ShouldReturnTheViewModel()
+        {
+            // Arrange
+            const int numberOfElements = 10;
+            IReadOnlyCollection<AgentViewModel> viewModels = new List<AgentViewModel>
+            {
+                new AgentViewModel(),
+                new AgentViewModel()
+            };
+            _propertyService.Setup(x => x.GetTopAsync(numberOfElements))
+                .Returns(Task.FromResult(viewModels));
+
+            // Act
+            var result = await _subject.Index() as ViewResult;
+
+            // Assert
+            result.Should().NotBeNull();
+            var model = result.Model as IReadOnlyCollection<AgentViewModel>;
+            model.Should().NotBeNull();
+            model.Count.Should().Be(viewModels.Count);
+            _propertyService.Verify(x => x.GetTopAsync(numberOfElements), Times.Once);
+        }
+    }
+}
